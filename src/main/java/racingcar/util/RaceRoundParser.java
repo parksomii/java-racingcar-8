4292135704f9dc811a17b4e1710c6e2b5028e526
parksomii <somi020605@gmail.com>
2025-10-24 17:42:50 +0900

    feat: 경주 라운드 수 파싱 유틸리티 클래스 추가

---
+++ src/main/java/racingcar/util/RaceRoundParser.java
@@ -0,0 +1,32 @@
+package racingcar.util;
+
+import racingcar.exception.ErrorMessage;
+import racingcar.exception.RacingCarException;
+
+public class RaceRoundParser {
+
+    private static final int MIN_ROUND_COUNT = 1;
+
+    /**
+     * 문자열로 입력된 라운드 수를 파싱합니다.
+     *
+     * @param rawRoundsToRace 파싱할 라운드 수 문자열
+     * @return 파싱된 라운드 수 (양수)
+     * @throws RacingCarException 입력이 유효하지 않은 경우
+     */
+    public static int parseRaceRound(String rawRoundsToRace) {
+        if (rawRoundsToRace == null || rawRoundsToRace.isBlank()) {
+            throw new RacingCarException(ErrorMessage.EMPTY_INPUT);
+        }
+
+        try {
+            int rounds = Integer.parseInt(rawRoundsToRace.trim());
+            if (rounds < MIN_ROUND_COUNT) {
+                throw new RacingCarException(ErrorMessage.RACE_ROUND_OUT_OF_BOUND);
+            }
+            return rounds;
+        } catch (NumberFormatException e) {
+            throw new RacingCarException(ErrorMessage.INVALID_NUMBER_FORMAT);
+        }
+    }
+}
